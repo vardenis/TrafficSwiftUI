6ae5c5f306bf0b716228de57456d607a16818931
Denis <varenikovda@gmail.com>
2022-06-07 21:18:45 +0300

    Initial Commit

---
+++ TrafficSwiftUI/TrafficSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficSwiftUIApp.swift
+//  TrafficSwiftUI
+//
+//  Created by Денис on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
